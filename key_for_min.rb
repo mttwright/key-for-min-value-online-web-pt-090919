e758f4f9418ebad43569d30a1f9d4d3f049bcec1
virtual-frame-7789 <mttwright@gmail.com>
2019-09-22 04:21:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,16 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     nil
   else
-    lowest_value = 0
+    lowest_value = 100000000000000000000000000000000000
+    the_key
     name_hash.collect do |key, value|
+      if value < lowest_value
+        lowest_value = value
+        the_key = key
+      end
       
   
     end
+    the_key
   end  
 end
